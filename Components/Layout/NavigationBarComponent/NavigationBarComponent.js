2c16f0091f66312718486398ac5de1efd96153cb
Alexander <ajveats@gmail.com>
2023-03-04 12:27:52 +0000

    Styling of general features

--- Components/Layout/NavigationBarComponent/NavigationBarComponent.js
+++ Components/Layout/NavigationBarComponent/NavigationBarComponent.js
@@ -1,19 +1,27 @@
 import Link from 'next/link';
 import styles from './navigationBarComponent.module.scss';
+import { faPersonWalking, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const NavigationBarComponent = () => {
     return (
         <div className={styles.navigationBarComponent}>
             <div className={styles.navigationLogoContainer}>
-
+                {/* <FontAwesomeIcon icon="fa-solid fa-person-walking" size="3x" /> */}
+                <FontAwesomeIcon icon={faPersonWalking} />
             </div>
             <div className={styles.navigationLinks}>
                 <Link href='/home'>Home</Link>
-                <Link href='/home'>Create</Link>
+                <Link href='/draw'>Create</Link>
                 <Link href='/home'>Explore</Link>
-                <Link href='/home'>Favourites</Link>
+                {/* <Link href='/home'>Favourites</Link> */}
                 <Link href='/home'>Your Walks</Link>
                 <Link href='/account'>Account</Link>
+            </div>
+            <div className={styles.navigationIcon}>
+                {/* <FontAwesomeIcon icon="fa-solid fa-person-walking" size="3x" /> */}
+                <FontAwesomeIcon icon={faEllipsis} />
             </div>
         </div>
     );
