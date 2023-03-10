d570a7c2dafea8eba5f77f6f9097f0bd797b5018
Alexander <ajveats@gmail.com>
2023-03-10 18:22:32 +0000

    Updated profile info component

--- Components/ProfileComponent/ProfileInfoComponent/ProfileInfoComponent.js
+++ Components/ProfileComponent/ProfileInfoComponent/ProfileInfoComponent.js
@@ -1,13 +1,13 @@
 import styles from './ProfileInfoComponent.module.scss';
 
 const ProfileInfoComponent = ({ user }) => {
-    console.log(user);
+    // console.log(user);
     return (
         <div>
-            <p className={styles.profileInfoComponent}>{user.firstname}</p>
-            <p className={styles.profileInfoComponent}>{user.lastname}</p>
-            <p className={styles.profileInfoComponent}>{user.location}</p>
-            <p className={styles.profileInfoComponent}>{user.bio}</p>
+            <p className={styles.profileInfoComponent}>Firstname: {user.firstname}</p>
+            <p className={styles.profileInfoComponent}>Lastname: {user.lastname}</p>
+            <p className={styles.profileInfoComponent}>Location: {user.location}</p>
+            <p className={styles.profileInfoComponent}>Bio: {user.bio}</p>
         </div>
     );
 }
