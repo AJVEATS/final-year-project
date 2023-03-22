16b543bf6901a59e2b7c5da24480690d27a51f72
Alexander <ajveats@gmail.com>
2023-03-22 12:51:41 +0000

    Updating layout

--- Components/Layout/LayoutComponent/LayoutComponent.js
+++ Components/Layout/LayoutComponent/LayoutComponent.js
@@ -4,32 +4,11 @@ import NavigationBarMobileComponent from '../NavigationBarMobileComponent.js/Nav
 import styles from './LayoutComponent.module.scss';
 
 const LayoutComponent = ({ children }) => {
-    // const [isScreenMobile, setIsScreenMobile] = useState(false);
-
-    // useEffect(() => {
-    //     const handleScreenResize = () => {
-    //         if (screen.width <= 1000) {
-    //             console.log('this is a mobile device');
-    //             setIsScreenMobile(true);
-    //         } else if (screen.width > 1000) {
-    //             console.log('this is not a mobile device');
-    //             setIsScreenMobile(false);
-    //         }
-    //         console.log(isScreenMobile);
-    //     }
-    //     handleScreenResize();
-    //     window.addEventListener('resize', handleScreenResize);
-    // }, []);
-
     return (
         <div className={styles.layoutComponent}>
-            {/* {isScreenMobile && <NavigationBarMobileComponent className={styles.navigationBarComponentMobile}/>}
-            {!isScreenMobile && <NavigationBarComponent className={styles.navigationBarComponent} />} */}
-
             <NavigationBarMobileComponent className={styles.navigationBarComponentMobile} />
             <NavigationBarComponent className={styles.navigationBarComponent} />
             <div className={styles.mainContentContainer}>
-                {/* <p className={styles.introText}>Good Afternoon Alex</p> */}
                 {children}
             </div>
         </div>
